c6e42d06eba4ec60ea3ea31acaadcaae4c643b64
Bradley Temple <bradley.temple@jpmchase.com>
2014-11-05 08:05:12 -0500

    Fixing delegation

--- jarvis/services/base_service.rb
+++ jarvis/services/base_service.rb
@@ -7,18 +7,20 @@ module Jarvis
       attr_accessor :message
       def initialize(message)
         # all attributes of the slack message will be available to the service class.
-        @message = SimpleDelegator.new(message)
+        @message = message
       end
 
       def run
         # All Services should implement this method to perform their designated action
         # Not implementing this method will result in nothing being run and return whatever comes from #say
-        # You should always return self as to not break the api endpoint
-        self
       end
 
       def say
         # All Services should implement this method to return what Jarvis says in respose
+      end
+
+      def method_missing(name, *args, &blk)
+        message.public_send(name, *args, &blk)
       end
     end
   end
