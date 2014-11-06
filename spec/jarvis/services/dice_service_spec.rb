fab049015ece44e9931d1600ee14d4bd8a627a5a
gregmalcolm <greg@gregmalcolm.com>
2014-11-06 12:32:55 -0500

    Do you have a test for that?

--- spec/jarvis/services/dice_service_spec.rb
+++ spec/jarvis/services/dice_service_spec.rb
@@ -14,7 +14,6 @@ describe Jarvis::Services::DiceService do
       subject { described_class.new(message) }
       before  { allow(Random).to receive_messages(rand: 20) }
       it { expect(subject.send(:roll)).to eq 20 }
-      
     end
   end
 end
