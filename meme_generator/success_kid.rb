6a08df744d7ea505e36dd30c1a5bbb07c1499d8d
Bradley Temple <bradley.temple@jpmchase.com>
2014-11-06 13:20:39 -0500

    1 or 2 lines

--- meme_generator/success_kid.rb
+++ meme_generator/success_kid.rb
@@ -10,9 +10,10 @@ module MemeGenerator
     end
 
     def tokenize
-      captures = request_text.match(/jarvis.+success kid(.[^,]+),?(.[^,]+)?/i).captures.map(&:strip)
-      self.text0 = captures[0]
-      self.text1 = captures[1]
+      captures = request_text.match(/jarvis.+success kid(.[^,]+),?(.[^,]+)?/i).captures.map {|t| t.strip if t }
+      self.text0 = captures[0] || ""
+      self.text1 = captures[1] || ""
+      true
     end
   end
 end
--- spec/meme_generator/success_kid_spec.rb
+++ spec/meme_generator/success_kid_spec.rb
@@ -1,12 +1,24 @@
 require 'spec_helper'
 
 describe MemeGenerator::SuccessKid do
-  subject { described_class.new("Jarvis success kid does this work, yes it does") }
-  before { allow(HTTParty).to receive_messages(:get => true) }
-  it "Tokenizes the input" do
-    subject.get
-    expect(subject.text0).to eq "does this work"
-    expect(subject.text1).to eq "yes it does"
+  context "two words" do
+    subject { described_class.new("Jarvis success kid does this work, yes it does") }
+    before { allow(HTTParty).to receive_messages(:get => true) }
+    it "Tokenizes the input" do
+      subject.get
+      expect(subject.text0).to eq "does this work"
+      expect(subject.text1).to eq "yes it does"
+    end
+  end
+
+  context "one line" do
+    subject { described_class.new("Jarvis success kid does this work yes it does") }
+    before { allow(HTTParty).to receive_messages(:get => true) }
+    it "Tokenizes the input" do
+      subject.get
+      expect(subject.text0).to eq "does this work yes it does"
+      expect(subject.text1).to eq ""
+    end
   end
 
 end
