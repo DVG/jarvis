cdf78d395c8aecd3c1cae3129f4873c54494a079
DVG <devryguy@gmail.com>
2014-11-20 12:29:13 -0500

    Try and fix not sure if

--- meme_generator/not_sure_if.rb
+++ meme_generator/not_sure_if.rb
@@ -6,7 +6,7 @@ module MemeGenerator
     end
 
     def tokenize
-      captures = request_text.match(/jarvis.+(not sure if.+),(or.+)/i).captures.map {|t| t.strip if t }
+      captures = request_text.match(/jarvis.+(not sure if.+),(.+)/i).captures.map {|t| t.strip if t }
       self.text0 = captures[0] || ""
       self.text1 = captures[1] || ""
       true
